c531da2088d6318943d308be13e273fdd0481f40
Phillip Inman <phillip.inman@shinesolutions.com>
2019-04-29 11:06:38 +1000

    More Debug

--- src/common/common.groovy
+++ src/common/common.groovy
@@ -8,6 +8,7 @@ package common
  * TODO: This download currently uses wget, but prolly best to replace it with
  * curl due to availability.
  */
+println "common.groovy"
 def httpDownload(script, url, destDir, fileName) {
   script.sh """
   wget ${url} -O ${destDir}/${fileName}
